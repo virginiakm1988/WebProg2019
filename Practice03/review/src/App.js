7d59932017b122aacf1da1b711a51c809c1b6bf1
jonathan <cjs840312@gmail.com>
2019-03-24 22:23:36 +0800

    add Practice03/review

--- Practice03/review/src/App.js
+++ Practice03/review/src/App.js
@@ -1,34 +1,87 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
-// css要記得去改
-import './styles.css';
+import logo from './logo.svg';
+import './App.css';
+import bgImage from "./image/bg.jpg";
+import bgImage2 from "./image/bg2.jpg";
 
-class Todolist extends Component {
-         render() {
+class Welcome extends Component {
+  render() {
+    let rec = this.props.scoreCard.records.map(e => 
+      <tr>{e.map(
+        g => <td>{g}</td>
+      )}</tr>
+    );
+    var sectionStyle = {
+      width: "100%",
+      height: "100%",
+    // makesure here is String确保这里是一个字符串，以下是es6写法
+      backgroundImage: `url(${bgImage})` 
+	};
 
-            return(
-                <div id={this.props.usedid.root[0]} class={this.props.usedid.root[1]}>
-                <header class={this.props.usedid.header[0]}><h1 class={this.props.usedid.header[1]}>{this.props.project.name}'s react list</h1></header>
-            
-                     
-            <section class={this.props.usedid.section[0]}><input class={this.props.usedid.section[1]} type={this.props.usedid.section[2]} id={this.props.usedid.section[3]} placeholder={this.props.project.placeholder}  ></input>
-               
-               <ul class={this.props.usedid.section[4]} id={this.props.usedid.section[5]}>
-               {this.props.project.todolist.map(e=><li class={this.props.usedid.list[0]}><div class={this.props.usedid.list[1]}><input type={this.props.usedid.list[2]} id={e}></input><label for={e}></label>
-               </div><h1 class={this.props.usedid.list[3]}>{e}</h1><img class={this.props.usedid.list[4]} ></img></li>)}
-               </ul>
-            </section>
-            <footer class={this.props.usedid.footer[0]} id={this.props.usedid.footer[1]}>
-                <div class={this.props.usedid.footer[2]} id={this.props.usedid.footer[3]}>{this.props.project.todolist.length} left</div>
-                <ul class={this.props.usedid.footer[4]}>{this.props.project.buttom.map(e=> <div class={this.props.usedid.footer[5]}><button>{e}</button></div>)}</ul>
-                <div class={this.props.usedid.footer[5]}>
-               <button id={this.props.usedid.footer[6]}>{this.props.project.last}</button></div>
-               
-          
-            </footer></div>
+	var sectionStyle2 = {
+		width: "100%",
+		height: "300px",
+	  // makesure here is String确保这里是一个字符串，以下是es6写法
+		backgroundImage: `url(${bgImage2})` 
+	  };
 
-            )
-         }
-        }
-        
-        export default Todolist;
+	var font = {
+		color:'white',
+		fontSize:40,
+		fontWeight:'bold',
+		textShadowColor:'#C0C0C0',
+		textShadowRadius:2,
+		textShadowOffset:{width:2,height:2},
+		textAlign:'center',
+		margin:50
+	};
+
+	var toBlock1 = function(){
+		window.location.hash = "#block1";
+	}
+	var toBlock2 = function(){
+		window.location.hash = "#block2";
+	}
+	var toBlock3 = function(){
+		window.location.hash = "#block3";
+	}
+	var toBlock4 = function(){
+		window.location.hash = "#block4";
+	}
+    return (
+    <body>
+    	<div style = {sectionStyle}>
+    		<table>
+      			<caption> {this.props.scoreCard.name}'s Score </caption>
+      			<thead>
+        			<tr>{ this.props.columnIndex.map(e => (
+        				<th>{e}</th>
+    				))}
+    				</tr>
+  				</thead>
+  				<tbody>
+    				{rec}
+  				</tbody>
+    		</table>
+    	</div>
+		<div id="menu">
+			<ul>
+				<li><a onMouseDown={toBlock1}>選單A</a></li>
+				<li><a onMouseDown={toBlock2}>選單B</a></li>
+				<li><a onMouseDown={toBlock3}>選單C</a></li>
+				<li><a onMouseDown={toBlock4}>選單D</a></li>
+			</ul>
+		</div>
+		{this.props.blockList.map(e => (
+			<div style={font} id={e}>
+				{e}
+				<div style = {sectionStyle2}>
+				</div>
+			</div>
+		))}
+    </body>
+    );
+  }
+}
+
+export default Welcome;
--- Practice03/review/src/index.js
+++ Practice03/review/src/index.js
@@ -1,26 +1,25 @@
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-// import App from './App';
-import Todolist  from './App';
+import './styles.css';
+import Welcome from './App';
 import * as serviceWorker from './serviceWorker';
 
-const project={name:'Brace',
-todolist:['Signal systems','ETS test','Algorithm','Web'],
-buttom:['There','are','no' ,'delete' ,'buttons'],
-last:'Life is but a span',placeholder:' You  have  several  tasks'};
-
-const usedid={
-root:['root','todo-app__root'],header:['todo-app__header','todo-app__title'],
-section:['todo-app__main','todo-app__input','text','todo-input','todo-app__list','todo-list'],
-list:['todo-app__item','todo-app__checkbox','checkbox','todo-app__item-detail','todo-app__item-x'],
-footer:['todo-app__footer','todo-footer','todo-app__total','count','todo-app__view-buttons','todo-app__clean',
-'clear']
-}
-ReactDOM.render( < 
-    Todolist project={project} usedid={usedid} / >,document.getElementById('root'));
+let n = "why?";
+const columnIndex = [ 'Subject', 'Score' ];
+const scoreCard = { 
+  name: 'Ric',
+  records: [
+    [ 'Math', 100 ],
+    [ 'Chinese', 87 ],
+    [ 'English', 100 ],
+    [ 'Science', 100 ],
+    [ 'Social', 0 ] 
+  ]
+};
+const blockList = ['block1', 'block2', 'block3', 'block4']
+ReactDOM.render(<Welcome scoreCard = {scoreCard}
+                        columnIndex = {columnIndex}
+                        blockList = {blockList}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
