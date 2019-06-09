b2c2571682570838865f4f911da4806bf167ae94
zihching chen <virginia.chen2007@gmail.com>
2019-06-09 10:52:36 +0800

    decorative like

--- Homework03/frontend/src/components/User.js
+++ Homework03/frontend/src/components/User.js
@@ -20,7 +20,8 @@ class User extends Component{
       name : this.props.data.name,
       age : this.props.data.age,
       email : this.props.data.email,
-      onClick: false
+      onClick: false,
+      like:0
     }
   };
 
@@ -51,7 +52,21 @@ class User extends Component{
         const filtered_post = data.posts.filter(post=>(post.author.name===this.state.name))
         const posts = filtered_post.map((post, id) => (
             <Card>
-              <CardBody>{post.title}</CardBody>
+              <CardBody>
+              <div>
+                {post.title}
+                
+                <svg onClick={
+                  click=>{
+                    console.log("clicked");
+                    this.setState({like:this.state.like+1})
+              }} style={{float : "right"}} className = "like"  version = "1.1" width= "24" height= "24" viewBox = "0 0 24 24" >
+                        <path d = "M23,10C23,8.89 22.1,8 21,8H14.68L15.64,3.43C15.66,3.33 15.67,3.22 15.67,3.11C15.67,2.7 15.5,2.32 15.23,2.05L14.17,1L7.59,7.58C7.22,7.95 7,8.45 7,9V19A2,2 0 0,0 9,21H18C18.83,21 19.54,20.5 19.84,19.78L22.86,12.73C22.95,12.5 23,12.26 23,12V10.08L23,10M1,21H5V9H1V21Z"></path>
+                  </svg>
+                
+              </div>
+              
+              </CardBody>
             </Card>
         ))
         if (!unsubscribe)
